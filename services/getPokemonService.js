06d13c314aba5306f0e83d96aefc220ba62b1f90
Waltavian Kelley <a.Babycane421@outlook.fr>
2025-11-23 19:41:16 -0600

    new repo init

---
+++ services/getPokemonService.js
@@ -0,0 +1,38 @@
+const baseUrl = 'https://pokeapi.co/api/v2';
+
+// Base function for API requests
+// A UTIL function
+async function makeAPIRequest(endpoint) {
+  try {
+    const response = await axios.get(endpoint);
+    const data = await response.data;
+
+    return {
+      success: true,
+      data: data
+    };
+  } catch (error) {
+    return {
+      success: false,
+      error: error
+    };
+  }
+}
+
+// Specific functions using the base function
+async function getPokemonAPI(pokemonName) {
+  const endpoint = `${baseUrl}/pokemon/${pokemonName}`;
+  return makeAPIRequest(endpoint);
+}
+
+async function getAbilitiesAPI() {
+  const endpoint = `${baseUrl}/ability`;
+  return makeAPIRequest(endpoint);
+}
+
+async function getItemAPI(itemId) {
+  const endpoint = `${baseUrl}/item/${itemId}`;
+  return makeAPIRequest(endpoint);
+}
+
+export { getPokemonAPI, getAbilitiesAPI, getItemAPI };
